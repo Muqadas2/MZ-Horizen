bab2cc8231ab94b76e11937332241ed2db7755ae
Muqadas Zahra <muqadas.zahra300@gmail.com>
2024-07-26 12:10:16 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
 // index page js
-function showsidebar(){
-         const sidebar=document.querySelector('.sidebar')
-         sidebar.style.display = 'flex';
-      }
-function hideSidebar(){
-         const sidebar=document.querySelector('.sidebar')
-         sidebar.style.display = 'none';
-      }
-// about page js      
+// THIS FUNCTION IS EXECUTED WHEN USER OPEN THE SIDE BAR 
+function showsidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex';
+}
+// THIS FUNCTION IS EXECUTED WHEN USER CLOSES THE SIDE BAR       
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none';
+}
+//                              HOME PAGE
+// Target the h1 element
+const h1Element = document.querySelector('.content h1');
+// Initial styles using JavaScript
+h1Element.style.opacity = 0;
+h1Element.style.transform = 'translateY(-20px)';
+h1Element.style.color = "blue";
+
+// Define the animation using GSAP
+gsap.to(h1Element, {
+    duration: 2,
+    opacity: 1,
+    y: 0,
+    ease: "power2.inOut",
+    repeat: -1,
+    yoyo: true,
+    delay: 1,
+    color: "red",
+    onStart: function () {
+        h1Element.style.color = "blue";
+    },
+    onComplete: function () {
+        h1Element.style.color = "black";
+    }
+});
+
+
+// about page js   
+function GotoHome() {
+    window.location.href = "index.html";
+}
 // GSAP timeline for sequential animation
 const tl = gsap.timeline({ defaults: { duration: 1, ease: "power2.inOut" } });
 
